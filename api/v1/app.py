46980178a1177d90706a930e9e6c3f507f4dbfbb
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-06-08 22:15:40 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,3 +8,15 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def db_teardown(exception):
+  """Function for teardown"""
+  storage.close()
+  
+@app.errorhandler(404)
+def 404_notfound(error):
+  return jsonify({"error": "Not Found"}), 404
+
+if __name__ == '__main__':
+  app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
