3a2c894e567ecc8bde9a8afb5ec87e383f02b3ba
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-06-08 20:05:11 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,21 @@
 """index file"""
 
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(stats)
