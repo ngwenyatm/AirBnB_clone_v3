646b0120b035a417eb27a7d7b0c64808e8ce6352
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-06-09 13:59:25 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,6 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+               
     def get(self,cls,id):
         """
             fetches one object, using classname and id
