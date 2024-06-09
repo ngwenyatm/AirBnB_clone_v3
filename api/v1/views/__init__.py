63a6687772ccb583bd703dbe4c2038bd6acbb8fc
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-06-10 00:01:53 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,6 @@ from api.v1.views.index import *
 from api.v1.views.states.py import *
 from api.v1.views.cities.py import *
 from api.v1.views.amenities.py import *
+from api.v1.views.users.py import *
+from api.v1.views.places.py import *
+from api.v1.views.places_reviews.py import *
