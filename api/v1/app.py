30a30f420af62fee4169174eba832ca6e0be340c
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-06-11 22:24:49 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Starts API
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
@@ -23,4 +23,6 @@ def notfound(error):
   return jsonify({"error": "Not Found"}), 404
 
 if __name__ == '__main__':
+  host = os.getenv("HBNB_API_HOST". "0.0.0.0")
+  port = int(os.getenv("HBNB_API_PORT", 5000))
   app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
